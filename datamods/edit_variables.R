dda0702094e41a2506192aa3976b389618accfa4
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2025-11-05 10:50:10 +0100

    added edit data variables

---
+++ datamods/edit_variables.R
@@ -0,0 +1,67 @@
+
+library(shiny)
+library(datamods)
+
+testdata <- data.frame(
+  date_as_char = as.character(Sys.Date() + 0:9),
+  date_as_num = as.numeric(Sys.Date() + 0:9),
+  datetime_as_char = as.character(Sys.time() + 0:9 * 3600*24),
+  datetime_as_num = as.numeric(Sys.time() + 0:9 * 3600*24),
+  num_as_char = as.character(1:10),
+  char = month.name[1:10],
+  char_na = c("A", "A", "B", NA, "B", "A", NA, "B", "A", "B"),
+  stringsAsFactors = FALSE
+)
+prevData <- testdata
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(version = 5L, preset = "bootstrap"),
+  tags$h3("Select, rename and convert variables"),
+  fluidRow(
+    column(
+      width = 6,
+      # radioButtons()
+      update_variables_ui("vars")
+    ),
+    column(
+      width = 6,
+      tags$b("original data:"),
+      verbatimTextOutput("original"),
+      verbatimTextOutput("original_str"),
+      tags$b("Modified data:"),
+      verbatimTextOutput("modified"),
+      verbatimTextOutput("modified_str")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+
+  rv_data <- reactiveVal(testdata)
+  rv_prevData <- reactiveVal(prevData)
+  updated_data <- update_variables_server(
+    id = "vars",
+    data = rv_data,
+    return_data_on_init = FALSE
+  )
+  output$original <- renderPrint({
+    rv_prevData()
+  })
+  output$original_str <- renderPrint({
+    str(rv_prevData())
+  })
+  observeEvent(updated_data(), {
+    rv_prevData(rv_data())
+    rv_data(updated_data())
+  })
+  output$modified <- renderPrint({
+    rv_data()
+  })
+  output$modified_str <- renderPrint({
+    str(rv_data())
+  })
+}
+
+if (interactive())
+  shinyApp(ui, server)
+shinyApp(ui, server)
